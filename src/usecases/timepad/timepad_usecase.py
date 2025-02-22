5de4d1efae86e058584a6e0936976c4b2fcb3898
Artem Bogatov <artembbogatov@yandex.ru>
2025-02-22 16:36:58 +0300

    fix

--- src/usecases/timepad/timepad_usecase.py
+++ src/usecases/timepad/timepad_usecase.py
@@ -10,6 +10,10 @@ from interface_adapters.repositories.base_file_repository import FileRepositoryP
 from interface_adapters.repositories.base_content_repository import (
     ContentRepositoryProtocol,
 )
+import logging
+
+
+logging.basicConfig(level=logging.INFO)
 
 
 class GetContentTimepadUseCase(AbstractUseCase):
@@ -26,54 +30,58 @@ class GetContentTimepadUseCase(AbstractUseCase):
         self.cities = {"Нижний Новгород": "nn"}
 
     def execute(self) -> list[ContentPydanticSchema]:
+        logging.info("Fetching content from Timepad")
         raw_content: list[dict] = self.gateway.fetch_content()
         contents = []
         unique_ids = self.content_repo.get_all_unique_ids()
 
         for content in raw_content:
+            logging.info(f"Processing content from Timepad {content}")
             unique_id = str(content.get("id")) + "timepad"
 
             if unique_id in unique_ids:
                 continue
-
-            image_url = "https:" + content.get("poster_image").get("uploadcare_url")
-            response = requests.get(image_url)
-            response.raise_for_status()
-            content_bytes = response.content
-            tags = [item.get("name") for item in content.get("categories")]
-            prices = [item.get("price") for item in content.get("ticket_types")]
-            processed_link_name = self.nlp_processor.generate_link_title(
-                content.get("description_short")
-            )
-            processed_categories = self.nlp_processor.determine_category(
-                content.get("description_short") + ",".join(tags)
-            )
-            contacts = [{processed_link_name: content.get("url", {})}]
-            datetime_obj = datetime.strptime(
-                content.get("starts_at"), "%Y-%m-%dT%H:%M:%S%z"
-            )
-            date_end = content.get("ends_at")
-            date_end = (
-                datetime.strptime(date_end, "%Y-%m-%dT%H:%M:%S%z") if date_end else None
-            )
-            city = content.get("location").get("city")
-            schema = ContentPydanticSchema(
-                name=content.get("name"),
-                description=content.get("description_short"),
-                tags=[processed_categories],
-                image=content_bytes,
-                contact=contacts,
-                date_start=datetime.strptime(
+            if content:
+                image_url = "https:" + content.get("poster_image").get("uploadcare_url")
+                response = requests.get(image_url)
+                response.raise_for_status()
+                content_bytes = response.content
+                tags = [item.get("name") for item in content.get("categories")]
+                prices = [item.get("price") for item in content.get("ticket_types")]
+                processed_link_name = self.nlp_processor.generate_link_title(
+                    content.get("description_short")
+                )
+                processed_categories = self.nlp_processor.determine_category(
+                    content.get("description_short") + ",".join(tags)
+                )
+                contacts = [{processed_link_name: content.get("url", {})}]
+                datetime_obj = datetime.strptime(
                     content.get("starts_at"), "%Y-%m-%dT%H:%M:%S%z"
-                ),
-                date_end=date_end,
-                time=datetime_obj.strftime("%H:%M"),
-                location=content.get("location").get("address"),
-                cost=min(prices),
-                city=self.cities.get(city),
-                unique_id=unique_id,
-            )
-            print(f"{schema=}")
-            contents.append(schema)
+                )
+                date_end = content.get("ends_at")
+                date_end = (
+                    datetime.strptime(date_end, "%Y-%m-%dT%H:%M:%S%z")
+                    if date_end
+                    else None
+                )
+                city = content.get("location").get("city")
+                schema = ContentPydanticSchema(
+                    name=content.get("name"),
+                    description=content.get("description_short"),
+                    tags=[processed_categories],
+                    image=content_bytes,
+                    contact=contacts,
+                    date_start=datetime.strptime(
+                        content.get("starts_at"), "%Y-%m-%dT%H:%M:%S%z"
+                    ),
+                    date_end=date_end,
+                    time=datetime_obj.strftime("%H:%M"),
+                    location=content.get("location").get("address"),
+                    cost=min(prices),
+                    city=self.cities.get(city),
+                    unique_id=unique_id,
+                )
+                logging.info(f"Saving content to database {schema}")
+                contents.append(schema)
         self.content_repo.save_content(contents)
         return contents
