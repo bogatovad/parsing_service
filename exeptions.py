a802640a72209cb9e2552c78c4ec1f59f60a0b3b
Кирилл Катранов <katranovrpk@gmail.com>
2025-03-12 14:05:51 +0300

    Review

---
+++ exeptions.py
@@ -0,0 +1,5 @@
+class ExeptionCheckAnswerKeys(Exception):
+    '''Вызывается когда нет ожидаемого ключа в ответе'''
+    pass
+
+
--- src/frameworks_and_drivers/django/parsing/data_manager/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DataManagerConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "frameworks_and_drivers.django.parsing.data_manager"
