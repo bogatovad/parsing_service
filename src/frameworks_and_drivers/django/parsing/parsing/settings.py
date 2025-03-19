cfa1b7adf90d75b92fa4396a65a28afab23712c6
Artem Bogatov <artembbogatov@yandex.ru>
2025-03-19 21:43:57 +0300

    fix

--- src/frameworks_and_drivers/django/parsing/parsing/settings.py
+++ src/frameworks_and_drivers/django/parsing/parsing/settings.py
@@ -157,16 +157,16 @@ AWS_S3_ENDPOINT_URL = MINIO_ENDPOINT
 
 
 CELERY_BEAT_SCHEDULE = {
-    "example-task-tg-10-05am": {
-        "task": "frameworks_and_drivers.django.parsing.data_manager.tasks.parsing_data_from_tg_task",
-        "schedule": crontab(hour="*/1"),
+    # "example-task-tg-10-05am": {
+    #     "task": "frameworks_and_drivers.django.parsing.data_manager.tasks.parsing_data_from_tg_task",
+    #     "schedule": crontab(hour="*/1"),
+    # },
+    "example-task-kudago-10-10am": {
+        "task": "frameworks_and_drivers.django.parsing.data_manager.tasks.parsing_data_from_kudago_task",
+        "schedule": crontab(hour=18, minute=47),
     },
-    # "example-task-kudago-10-10am": {
-    #     "task": "frameworks_and_drivers.django.parsing.data_manager.tasks.parsing_data_from_kudago_task",
-    #     "schedule": crontab(hour=9, minute=33),
-    # },
-    # "example-task-timepad-10-10am": {
-    #     "task": "frameworks_and_drivers.django.parsing.data_manager.tasks.parsing_data_from_timepad_task",
-    #     "schedule": crontab(hour=11, minute=50),
-    # },
+    "example-task-timepad-10-10am": {
+        "task": "frameworks_and_drivers.django.parsing.data_manager.tasks.parsing_data_from_timepad_task",
+        "schedule": crontab(hour=18, minute=55),
+    },
 }
