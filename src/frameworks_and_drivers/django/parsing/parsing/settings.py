7a6ebcb66c89907aaa1d03e854010e1810d66c36
Artem Bogatov <artembbogatov@yandex.ru>
2025-04-11 16:58:38 +0300

    fix

--- src/frameworks_and_drivers/django/parsing/parsing/settings.py
+++ src/frameworks_and_drivers/django/parsing/parsing/settings.py
@@ -157,16 +157,16 @@ AWS_S3_ENDPOINT_URL = MINIO_ENDPOINT
 
 
 CELERY_BEAT_SCHEDULE = {
-    "example-task-tg-10-05am": {
-        "task": "frameworks_and_drivers.django.parsing.data_manager.tasks.parsing_data_from_tg_task",
-        "schedule": crontab(minute="*/5"),
-    },
+    # "example-task-tg-10-05am": {
+    #     "task": "frameworks_and_drivers.django.parsing.data_manager.tasks.parsing_data_from_tg_task",
+    #     "schedule": crontab(minute="*/5"),
+    # },
     "example-task-kudago-10-10am": {
         "task": "frameworks_and_drivers.django.parsing.data_manager.tasks.parsing_data_from_kudago_task",
-        "schedule": crontab(hour=15, minute=40),
+        "schedule": crontab(hour=14, minute=10),
     },
     "example-task-timepad-10-10am": {
         "task": "frameworks_and_drivers.django.parsing.data_manager.tasks.parsing_data_from_timepad_task",
-        "schedule": crontab(hour=16, minute=40),
+        "schedule": crontab(hour=14, minute=40),
     },
 }
