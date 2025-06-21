8334a15ea67dc3dd7fda2a0a81e7e75f27e55987
Artem Bogatov <artembbogatov@yandex.ru>
2025-06-21 21:53:42 +0300

    feat: merge

--- src/frameworks_and_drivers/django/parsing/celery_app.py
+++ src/frameworks_and_drivers/django/parsing/celery_app.py
@@ -2,6 +2,11 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
+# Явно импортируем задачи из tasks.py
+from frameworks_and_drivers.django.parsing.tasks import (
+    delete_outdated_events,
+)  # Это зарегистрирует задачу
+
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE", "frameworks_and_drivers.django.parsing.parsing.settings"
 )
@@ -15,3 +20,5 @@ app.autodiscover_tasks()
 
 # Также убедимся что задачи зарегистрированы
 app.autodiscover_tasks(["frameworks_and_drivers.django.parsing"])
+
+print(delete_outdated_events)
