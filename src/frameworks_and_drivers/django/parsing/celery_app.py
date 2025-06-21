950462ebd51c8ddf00ac68dba7352b3df6883196
Artem Bogatov <artembbogatov@yandex.ru>
2025-06-21 13:08:13 +0300

    feat: add crontab tasks.

--- src/frameworks_and_drivers/django/parsing/celery_app.py
+++ src/frameworks_and_drivers/django/parsing/celery_app.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from frameworks_and_drivers.django.parsing import celery_tasks
 
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE", "frameworks_and_drivers.django.parsing.parsing.settings"
@@ -12,6 +13,7 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 
 # Автодискавери задач из Django приложений
 app.autodiscover_tasks()
+print(celery_tasks)
 
 # Явно импортируем модуль с задачами - это критически важно!
 
